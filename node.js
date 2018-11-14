0aae301acc2186fe72c5620514e984eb92c0f46e
viDit <f20180733@goa.bits-pilani.ac.in>
2018-11-14 14:31:26 +0530

    v1.2 not working

--- node.js
+++ node.js
@@ -1,7 +1,6 @@
 var mongodb =require('mongodb');
 var MongoClient= mongodb.MongoClient;
 var url = 'mongodb://127.0.0.1:27017/mydb';
-var mongoose = require('mongoose');
 var express = require('express');
 var bodyParser =require('body-parser');
 var app=express(); 
@@ -11,43 +10,36 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/hello");
-
-var nameSchema = new mongoose.Schema({
-	note:String,
-	
-});
-
-var User =mongoose.model("User", nameSchema);
-
-
 app.get("/",(req,res) => {
 	res.sendFile(__dirname + "/note.html");
 });
 
+var dbo
 
-
-
-MongoClient.connect(url, function(err, db) {
-  // if (err) throw err;
-  var dbo = db1.db("mydb");
-  dbo.createCollection("Notes", function(err, res) {
-    // if (err) throw err;
-    console.log("Collection created!");
-    db1.close();
+MongoClient.connect(url, function(err, db1) {
+  if (err) return err;
+      var dbo = db1.db("mydb");
+      dbo.createCollection("Notes", function(err, res) {
+  if (err) return err;
+      console.log("Collection created!");
+      db1.close();
   });
 }); 
 
-
-app.post("/Notes",(req,res) => {
-  dbo.collection('Notes').find().toArray((err, result) => {
+app.post('/Notes', (req, res) => {
+  dbo.collection('Notes').save(req.body, (err, result) => {
     if (err) return console.log(err)
-    
-    res.render('note.html', {notes: result})
-  });
-});
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
+
+app.get('/', (req, res) => {
+  db.collection('quotes').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    res.render('index.ejs', {quotes: result})
+  })
+})
 
 
 app.listen(port,() => {
