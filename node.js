d3f8212fea07b64f679ddfe15174220b00dd08bb
viDit <f20180733@goa.bits-pilani.ac.in>
2018-11-17 19:15:10 +0530

    v1.3 last step left

--- node.js
+++ node.js
@@ -5,40 +5,57 @@ var express = require('express');
 var bodyParser =require('body-parser');
 var app=express(); 
 var port = 3000;
+var path = require('path');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname,'public')));
 
 app.get("/",(req,res) => {
-	res.sendFile(__dirname + "/note.html");
+	res.sendFile(__dirname + "/public/note.html");
+	
+	// document.getElementById("output").innerHTML= "<b>vbfgfbg</b>";
+  	
+  	MongoClient.connect(url, function(err, db1) {
+    if (err) return err;
+        var dbo = db1.db("mydb");
+  		dbo.collection('Notes').find().toArray((err, result) => {
+    if (err) return console.log(err);
+    // res.render('index.ejs', {Notes: result});
+    
+   		console.log(result);
+  })
+})
 });
 
-var dbo
-
-MongoClient.connect(url, function(err, db1) {
-  if (err) return err;
-      var dbo = db1.db("mydb");
-      dbo.createCollection("Notes", function(err, res) {
-  if (err) return err;
-      console.log("Collection created!");
-      db1.close();
-  });
-}); 
-
 app.post('/Notes', (req, res) => {
-  dbo.collection('Notes').save(req.body, (err, result) => {
-    if (err) return console.log(err)
-    console.log('saved to database')
-    res.redirect('/')
+  MongoClient.connect(url, function(err, db1) {
+    if (err) return err;
+        var dbo = db1.db("mydb");
+        dbo.collection('Notes').save(req.body, (err, result) => {
+    if (err) return console.log(err);
+        console.log('saved to database');
+        return res.redirect('/');
+
+
+ 
+      // if (err) return err;
+      //   dbo.collection('Notes').find().toArray((err, result) => {
+      // if (err) return console.log(err);
+      //   res.render('index.ejs', {Notes: result});
+      //   console.log(result);
+      // })
+
+
+
   })
+  }); 
 })
 
 app.get('/', (req, res) => {
-  db.collection('quotes').find().toArray((err, result) => {
-    if (err) return console.log(err)
-    res.render('index.ejs', {quotes: result})
-  })
+
+  
 })
 
 
